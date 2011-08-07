5e45a3b2c97aef7762de75a52bdfd0e10911d89d
Will Sobel <will@systeminsights.com>
2011-08-06 23:02:50 -0700

    Added software version as first line printed to console with build timestamp.

--- agent/service.cpp
+++ agent/service.cpp
@@ -1,7 +1,41 @@
+/*
+ * Copyright (c) 2008, AMT – The Association For Manufacturing Technology (“AMT”)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the AMT nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * DISCLAIMER OF WARRANTY. ALL MTCONNECT MATERIALS AND SPECIFICATIONS PROVIDED
+ * BY AMT, MTCONNECT OR ANY PARTICIPANT TO YOU OR ANY PARTY ARE PROVIDED "AS IS"
+ * AND WITHOUT ANY WARRANTY OF ANY KIND. AMT, MTCONNECT, AND EACH OF THEIR
+ * RESPECTIVE MEMBERS, OFFICERS, DIRECTORS, AFFILIATES, SPONSORS, AND AGENTS
+ * (COLLECTIVELY, THE "AMT PARTIES") AND PARTICIPANTS MAKE NO REPRESENTATION OR
+ * WARRANTY OF ANY KIND WHATSOEVER RELATING TO THESE MATERIALS, INCLUDING, WITHOUT
+ * LIMITATION, ANY EXPRESS OR IMPLIED WARRANTY OF NONINFRINGEMENT,
+ * MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE. 
+ 
+ * LIMITATION OF LIABILITY. IN NO EVENT SHALL AMT, MTCONNECT, ANY OTHER AMT
+ * PARTY, OR ANY PARTICIPANT BE LIABLE FOR THE COST OF PROCURING SUBSTITUTE GOODS
+ * OR SERVICES, LOST PROFITS, LOSS OF USE, LOSS OF DATA OR ANY INCIDENTAL,
+ * CONSEQUENTIAL, INDIRECT, SPECIAL OR PUNITIVE DAMAGES OR OTHER DIRECT DAMAGES,
+ * WHETHER UNDER CONTRACT, TORT, WARRANTY OR OTHERWISE, ARISING IN ANY WAY OUT OF
+ * THIS AGREEMENT, USE OR INABILITY TO USE MTCONNECT MATERIALS, WHETHER OR NOT
+ * SUCH PARTY HAD ADVANCE NOTICE OF THE POSSIBILITY OF SUCH DAMAGES.
+ */
+
 #include "service.hpp"
 #include "string.h"
 #include <fstream>
 #include "dlib/logger.h"
+#include "version.h"
 
 static dlib::logger sLogger("init.service");
 
@@ -19,6 +53,13 @@ mIsService(false), mIsDebug(false)
 
 void MTConnectService::initialize(int aArgc, const char *aArgv[])
 {
+}
+
+static void printVersion()
+{
+  printf("MTConnect Agent Version %d.%d.%d.%d - " __TIMESTAMP__ "\n", 
+         AGENT_VERSION_MAJOR, AGENT_VERSION_MINOR, AGENT_VERSION_PATCH,
+         AGENT_VERSION_BUILD);
 }
 
 #ifdef WIN32
@@ -60,7 +101,7 @@ static void agent_termination_handler()
 int MTConnectService::main(int argc, const char *argv[]) 
 {
   std::set_terminate(agent_termination_handler);
-  
+  printVersion();
   try 
   {
     // If command-line parameter is "install", install the service. If debug or run
@@ -610,6 +651,7 @@ void MTConnectService::daemonize()
 
 int MTConnectService::main(int argc, const char *argv[]) 
 {
+  printVersion();
   if(argc > 1) {
     if (strcasecmp( argv[1], "help") == 0 || strncmp(argv[1], "-h", 2) == 0)
     {
---
+++ test/version.h
@@ -0,0 +1,5 @@
+// the configured options and settings for Tutorial
+#define AGENT_VERSION_MAJOR 0
+#define AGENT_VERSION_MINOR 0
+#define AGENT_VERSION_PATCH 0
+#define AGENT_VERSION_BUILD 0
