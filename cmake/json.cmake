97e4015941d77d5959af5e980bed9927effef5d2
Will Sobel <will@systeminsights.com>
2019-03-16 00:15:43 -0700

    JSON is header only, do not need to add dir.

--- cmake/json.cmake
+++ cmake/json.cmake
@@ -1,7 +1,6 @@
 
 
 set(JSON_BuildTests OFF CACHE INTERNAL "")
-add_subdirectory(json)
 
 function(AddJsonSupport projectTarget)
 	target_include_directories(${projectTarget} PRIVATE ${CMAKE_SOURCE_DIR}/json/include)
