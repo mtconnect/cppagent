08911ed61f39ccd7a03674850eaf9cae7b9b4edc
Lorenz Haas <lorenz.haas@histomatics.de>
2019-10-23 20:01:25 +0300

    Format CMake files and wrap paths in quotes
    I tried to find the most common used pattern:
    - Indent is 2 spaces, no tabs
    - Max line length is 100
    - Comments above command not at the end of a line.
    - No space between command and bracket: if(...) instead of if (...)
    - No space after/before brackets: if(...) instead of if( ... )
    - Wrap paths in quotes (handle paths with spaces correctly)
    - Do not repeat condition in endif()
    - Wrapped command have the closing bracket on the last line
    - Only one new line at end of a file
    - Do not wrap commands if they fit into one line
    - CMake commands are lowercase
    - For target_include_directories() etc. it is enough to specify PRIVATE
    once

--- cmake/msvc_use_static_runtime.cmake
+++ cmake/msvc_use_static_runtime.cmake
@@ -5,13 +5,12 @@
 # Change these to use the static variants 'MT' and 'MTd'
 #
 # Note: This must be done before adding any CMake targets (executables, libraries etc...)
-#
+
 if(MSVC)
-	foreach(flag_var
-			CMAKE_CXX_FLAGS CMAKE_CXX_FLAGS_DEBUG CMAKE_CXX_FLAGS_RELEASE
-			CMAKE_CXX_FLAGS_MINSIZEREL CMAKE_CXX_FLAGS_RELWITHDEBINFO)
-		if(${flag_var} MATCHES "/MD")
-			string(REGEX REPLACE "/MD" "/MT" ${flag_var} "${${flag_var}}")
-		endif()
-	endforeach()
+  foreach(flag_var CMAKE_CXX_FLAGS CMAKE_CXX_FLAGS_DEBUG CMAKE_CXX_FLAGS_RELEASE
+    CMAKE_CXX_FLAGS_MINSIZEREL CMAKE_CXX_FLAGS_RELWITHDEBINFO)
+    if(${flag_var} MATCHES "/MD")
+      string(REGEX REPLACE "/MD" "/MT" ${flag_var} "${${flag_var}}")
+    endif()
+  endforeach()
 endif()
--- cmake/osx_no_app_or_frameworks.cmake
+++ cmake/osx_no_app_or_frameworks.cmake
@@ -1,11 +1,7 @@
-# Change how CMake find_* commands choose between OS X Application Bundles and unix-style package components.
+# Change how CMake find_* commands choose between OS X Application Bundles and unix-style package
+# components.
 # We will always prefer packages
-#
 set(CMAKE_FIND_APPBUNDLE NEVER)
-
-# Change how CMake find_* commands choose between OS X frameworks and unix-style package components.
-# We will always prefer packages
-#
 set(CMAKE_FIND_FRAMEWORK NEVER FORCE)
 
 # We need to use the CLang C++14 compiler for the agent on OS X since we are using the
@@ -13,5 +9,4 @@ set(CMAKE_FIND_FRAMEWORK NEVER FORCE)
 if (APPLE)
   set(CMAKE_XCODE_ATTRIBUTE_CLANG_CXX_LANGUAGE_STANDARD "c++14")
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-unused-local-typedef -Wno-deprecated-declarations -std=c++14")
-endif(APPLE)
-
+endif()
