4426e7d239aa8397e0b84b222cf6cb39aae50e5c
Lorenz Haas <lorenz.haas@histomatics.de>
2019-10-27 20:44:18 +0200

    Convert component_test

--- test/component_test.cpp
+++ test/component_test.cpp
@@ -15,200 +15,159 @@
 //    limitations under the License.
 //
 
-#include "Cuti.h"
+#include "gtest/gtest.h"
 
-#include "device.hpp"
 #include "component.hpp"
+#include "device.hpp"
 
 using namespace std;
 using namespace mtconnect;
 
-TEST_CLASS(ComponentTest)
-{
-protected:
+class ComponentTest : public testing::Test {
+ protected:
+  void SetUp() override {
+    std::map<string, string> attributes1;
+    attributes1["id"] = "1";
+    attributes1["name"] = "ComponentTest1";
+    attributes1["nativeName"] = "NativeName";
+    attributes1["uuid"] = "UnivUniqId1";
+    m_compA = new mtconnect::Component("Axes", attributes1);
+
+    std::map<string, string> attributes2;
+    attributes2["id"] = "3";
+    attributes2["name"] = "ComponentTest2";
+    attributes2["uuid"] = "UnivUniqId2";
+    attributes2["sampleRate"] = "123.4";
+    m_compB = new mtconnect::Component("Controller", attributes2);
+  }
+
+  void TearDown() override {
+    if (m_compA != nullptr) {
+      delete m_compA;
+      m_compA = nullptr;
+    }
+    if (m_compB != nullptr) {
+      delete m_compB;
+      m_compB = nullptr;
+    }
+  }
+
   mtconnect::Component *m_compA;
   mtconnect::Component *m_compB;
-  
-public:
-  void testGetters();
-  void testGetAttributes();
-  void testDescription();
-  void testRelationships();
-  void testDataItems();
-  void testReferences();
-  
-  SET_UP();
-  TEAR_DOWN();
-  
-  CPPUNIT_TEST_SUITE(ComponentTest);
-  CPPUNIT_TEST(testGetters);
-  CPPUNIT_TEST(testGetAttributes);
-  CPPUNIT_TEST(testDescription);
-  CPPUNIT_TEST(testRelationships);
-  CPPUNIT_TEST(testDataItems);
-  CPPUNIT_TEST(testReferences);
-  CPPUNIT_TEST_SUITE_END();
 };
 
-// Registers the fixture into the 'registry'
-CPPUNIT_TEST_SUITE_REGISTRATION(ComponentTest);
+TEST_F(ComponentTest, Getters) {
+  ASSERT_EQ((string) "Axes", m_compA->getClass());
+  ASSERT_EQ((string) "1", m_compA->getId());
+  ASSERT_EQ((string) "ComponentTest1", m_compA->getName());
+  ASSERT_EQ((string) "UnivUniqId1", m_compA->getUuid());
+  ASSERT_EQ((string) "NativeName", m_compA->getNativeName());
 
-// ComponentTest public methods
-void ComponentTest::setUp()
-{
-  std::map<string, string> attributes1;
-  attributes1["id"] = "1";
-  attributes1["name"] = "ComponentTest1";
-  attributes1["nativeName"] = "NativeName";
-  attributes1["uuid"] = "UnivUniqId1";
-  m_compA = new mtconnect::Component("Axes", attributes1);
-  
-  std::map<string, string> attributes2;
-  attributes2["id"] = "3";
-  attributes2["name"] = "ComponentTest2";
-  attributes2["uuid"] = "UnivUniqId2";
-  attributes2["sampleRate"] = "123.4";
-  m_compB = new mtconnect::Component("Controller", attributes2);
+  ASSERT_EQ((string) "Controller", m_compB->getClass());
+  ASSERT_EQ((string) "3", m_compB->getId());
+  ASSERT_EQ((string) "ComponentTest2", m_compB->getName());
+  ASSERT_EQ((string) "UnivUniqId2", m_compB->getUuid());
+  ASSERT_TRUE(m_compB->getNativeName().empty());
 }
 
-
-void ComponentTest::tearDown()
-{
-  if (m_compA != nullptr) {
-    delete m_compA; m_compA = nullptr;
-  }
-  if (m_compB != nullptr) {
-    delete m_compB; m_compB = nullptr;
-  }
-}
-
-
-void ComponentTest::testGetters()
-{
-  CPPUNIT_ASSERT_EQUAL((string) "Axes", m_compA->getClass());
-  CPPUNIT_ASSERT_EQUAL((string) "1", m_compA->getId());
-  CPPUNIT_ASSERT_EQUAL((string) "ComponentTest1", m_compA->getName());
-  CPPUNIT_ASSERT_EQUAL((string) "UnivUniqId1", m_compA->getUuid());
-  CPPUNIT_ASSERT_EQUAL((string) "NativeName", m_compA->getNativeName());
-  
-  CPPUNIT_ASSERT_EQUAL((string) "Controller", m_compB->getClass());
-  CPPUNIT_ASSERT_EQUAL((string) "3", m_compB->getId());
-  CPPUNIT_ASSERT_EQUAL((string) "ComponentTest2", m_compB->getName());
-  CPPUNIT_ASSERT_EQUAL((string) "UnivUniqId2", m_compB->getUuid());
-  CPPUNIT_ASSERT(m_compB->getNativeName().empty());
-}
-
-
-void ComponentTest::testGetAttributes()
-{
+TEST_F(ComponentTest, GetAttributes) {
   const auto &attributes1 = m_compA->getAttributes();
-  
-  CPPUNIT_ASSERT_EQUAL((string) "1", attributes1.at("id"));
-  CPPUNIT_ASSERT_EQUAL((string) "ComponentTest1", attributes1.at("name"));
-  CPPUNIT_ASSERT_EQUAL((string) "UnivUniqId1", attributes1.at("uuid"));
-  CPPUNIT_ASSERT(attributes1.find("sampleRate") == attributes1.end());
-  
+
+  ASSERT_EQ((string) "1", attributes1.at("id"));
+  ASSERT_EQ((string) "ComponentTest1", attributes1.at("name"));
+  ASSERT_EQ((string) "UnivUniqId1", attributes1.at("uuid"));
+  ASSERT_TRUE(attributes1.find("sampleRate") == attributes1.end());
+
   const auto &attributes2 = m_compB->getAttributes();
-  
-  CPPUNIT_ASSERT_EQUAL((string) "3", attributes2.at("id"));
-  CPPUNIT_ASSERT_EQUAL((string) "ComponentTest2", attributes2.at("name"));
-  CPPUNIT_ASSERT_EQUAL((string) "UnivUniqId2", attributes2.at("uuid"));
-  CPPUNIT_ASSERT_EQUAL((string) "123.4", attributes2.at("sampleInterval"));
+
+  ASSERT_EQ((string) "3", attributes2.at("id"));
+  ASSERT_EQ((string) "ComponentTest2", attributes2.at("name"));
+  ASSERT_EQ((string) "UnivUniqId2", attributes2.at("uuid"));
+  ASSERT_EQ((string) "123.4", attributes2.at("sampleInterval"));
 }
 
-
-void ComponentTest::testDescription()
-{
+TEST_F(ComponentTest, Description) {
   map<string, string> attributes;
   attributes["manufacturer"] = "MANUFACTURER";
   attributes["serialNumber"] = "SERIAL_NUMBER";
-  
+
   m_compA->addDescription((string) "Machine 1", attributes);
   auto description1 = m_compA->getDescription();
-  
-  CPPUNIT_ASSERT_EQUAL((string) "MANUFACTURER", description1["manufacturer"]);
-  CPPUNIT_ASSERT_EQUAL((string) "SERIAL_NUMBER", description1["serialNumber"]);
-  CPPUNIT_ASSERT(description1["station"].empty());
-  CPPUNIT_ASSERT_EQUAL((string) "Machine 1", m_compA->getDescriptionBody());
-  
+
+  ASSERT_EQ((string) "MANUFACTURER", description1["manufacturer"]);
+  ASSERT_EQ((string) "SERIAL_NUMBER", description1["serialNumber"]);
+  ASSERT_TRUE(description1["station"].empty());
+  ASSERT_EQ((string) "Machine 1", m_compA->getDescriptionBody());
+
   attributes["station"] = "STATION";
   m_compB->addDescription((string) "", attributes);
   auto description2 = m_compB->getDescription();
-  
-  CPPUNIT_ASSERT_EQUAL((string) "MANUFACTURER", description2["manufacturer"]);
-  CPPUNIT_ASSERT_EQUAL((string) "SERIAL_NUMBER", description2["serialNumber"]);
-  CPPUNIT_ASSERT_EQUAL((string) "STATION", description2["station"]);
-  CPPUNIT_ASSERT(m_compB->getDescriptionBody().empty());
-  
+
+  ASSERT_EQ((string) "MANUFACTURER", description2["manufacturer"]);
+  ASSERT_EQ((string) "SERIAL_NUMBER", description2["serialNumber"]);
+  ASSERT_EQ((string) "STATION", description2["station"]);
+  ASSERT_TRUE(m_compB->getDescriptionBody().empty());
 }
 
-
-void ComponentTest::testRelationships()
-{
+TEST_F(ComponentTest, Relationships) {
   // Test get/set parents
   map<string, string> dummy;
-  mtconnect::Component *linear =
-    new mtconnect::Component("Linear", dummy);
-  
+  mtconnect::Component *linear = new mtconnect::Component("Linear", dummy);
+
   m_compA->setParent(*linear);
-  CPPUNIT_ASSERT(linear == m_compA->getParent());
-  
+  ASSERT_TRUE(linear == m_compA->getParent());
+
   Device *device = new Device(dummy);
   auto devPointer = dynamic_cast<mtconnect::Component *>(device);
-  
-  CPPUNIT_ASSERT(devPointer);
+
+  ASSERT_TRUE(devPointer);
   linear->setParent(*devPointer);
-  CPPUNIT_ASSERT(devPointer == linear->getParent());
-  
+  ASSERT_TRUE(devPointer == linear->getParent());
+
   // Test get device
-  CPPUNIT_ASSERT(device == m_compA->getDevice());
-  CPPUNIT_ASSERT(device == linear->getDevice());
-  CPPUNIT_ASSERT(device == device->getDevice());
-  
+  ASSERT_TRUE(device == m_compA->getDevice());
+  ASSERT_TRUE(device == linear->getDevice());
+  ASSERT_TRUE(device == device->getDevice());
+
   // Test add/get children
-  CPPUNIT_ASSERT(m_compA->getChildren().empty());
-  
+  ASSERT_TRUE(m_compA->getChildren().empty());
+
   mtconnect::Component *axes = new mtconnect::Component("Axes", dummy),
-           *thermostat = new mtconnect::Component("Thermostat", dummy);
+                       *thermostat = new mtconnect::Component("Thermostat", dummy);
   m_compA->addChild(*axes);
   m_compA->addChild(*thermostat);
-  
-  CPPUNIT_ASSERT_EQUAL((size_t) 2, m_compA->getChildren().size());
-  CPPUNIT_ASSERT(axes == m_compA->getChildren().front());
-  CPPUNIT_ASSERT(thermostat == m_compA->getChildren().back());
-  
+
+  ASSERT_EQ((size_t)2, m_compA->getChildren().size());
+  ASSERT_TRUE(axes == m_compA->getChildren().front());
+  ASSERT_TRUE(thermostat == m_compA->getChildren().back());
+
   delete device;
-  
+
   m_compA = nullptr;
   m_compB = nullptr;
 }
 
+TEST_F(ComponentTest, DataItems) {
+  ASSERT_TRUE(m_compA->getDataItems().empty());
 
-void ComponentTest::testDataItems()
-{
-  CPPUNIT_ASSERT(m_compA->getDataItems().empty());
-  
   map<string, string> dummy;
-  
-  DataItem *data1 = new DataItem(dummy),
-           *data2 = new DataItem(dummy);
+
+  DataItem *data1 = new DataItem(dummy), *data2 = new DataItem(dummy);
   m_compA->addDataItem(*data1);
   m_compA->addDataItem(*data2);
-  
-  CPPUNIT_ASSERT_EQUAL((size_t) 2, m_compA->getDataItems().size());
-  CPPUNIT_ASSERT(data1 == m_compA->getDataItems().front());
-  CPPUNIT_ASSERT(data2 == m_compA->getDataItems().back());
+
+  ASSERT_EQ((size_t)2, m_compA->getDataItems().size());
+  ASSERT_TRUE(data1 == m_compA->getDataItems().front());
+  ASSERT_TRUE(data2 == m_compA->getDataItems().back());
 }
 
-
-void ComponentTest::testReferences()
-{
+TEST_F(ComponentTest, References) {
   string id("a"), name("xxx");
   mtconnect::Component::Reference ref(id, name, mtconnect::Component::Reference::DATA_ITEM);
-  
+
   m_compA->addReference(ref);
-  CPPUNIT_ASSERT_EQUAL((size_t) 1, m_compA->getReferences().size());
-  
-  CPPUNIT_ASSERT_EQUAL((string) "xxx", m_compA->getReferences().front().m_name);
-  CPPUNIT_ASSERT_EQUAL((string) "a", m_compA->getReferences().front().m_id);
+  ASSERT_EQ((size_t)1, m_compA->getReferences().size());
+
+  ASSERT_EQ((string) "xxx", m_compA->getReferences().front().m_name);
+  ASSERT_EQ((string) "a", m_compA->getReferences().front().m_id);
 }
