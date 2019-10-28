9722382eaaf825b1f3b86bb6d5e099dee18c4c30
Lorenz Haas <lorenz.haas@histomatics.de>
2019-10-28 19:10:42 +0200

    Convert json_printer_asset_test

--- test/json_helper.hpp
+++ test/json_helper.hpp
@@ -1,18 +1,10 @@
-
-constexpr inline nlohmann::json::size_type operator "" _S(unsigned long long v)
-{
+constexpr inline nlohmann::json::size_type operator"" _S(unsigned long long v) {
   return static_cast<nlohmann::json::size_type>(v);
 }
 
-inline std::string operator "" _S(const char *v, std::size_t)
-{
-  return std::string(v);
-}
+inline std::string operator"" _S(const char *v, std::size_t) { return std::string(v); }
 
-
-static inline nlohmann::json find(nlohmann::json &array, const char *path, const char *value)
-{
-  //CPPUNIT_ASSERT(array.is_array());
+static inline nlohmann::json find(nlohmann::json &array, const char *path, const char *value) {
   nlohmann::json::json_pointer pointer(path);
   nlohmann::json res;
   for (auto &item : array) {
