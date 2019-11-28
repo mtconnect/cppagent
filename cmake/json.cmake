a5f840522ac69e4982c5619123611768c6479be0
Lorenz Haas <lorenz.haas@histomatics.de>
2019-11-28 22:31:58 +0100

    Modernize CMake with self describing targets

--- cmake/json.cmake
+++ cmake/json.cmake
@@ -1,7 +1,24 @@
-set(JSON_BuildTests OFF CACHE INTERNAL "")
+####################################################################################################
+#                    Providing JSON library - https://nlohmann.github.io/json                      #
+#                                                                                                  #
+# To use the JSON library simply link to it: target_link_libraries(<project> PRIVATE json).        #
+# This automatically sets all required information such as include directories, definitions etc.   #
+####################################################################################################
 
-function(AddJsonSupport projectTarget)
-  target_include_directories(${projectTarget} PRIVATE "${CMAKE_SOURCE_DIR}/json/include")
-endfunction()
+set(agent_json_lib_version_tag "v3.7.3")
 
+if(NOT TARGET json)
+  message(STATUS "Make JSON library (${agent_json_lib_version_tag}) available.")
+  message(STATUS "  * Donwload JSON library")
+  include(FetchContent)
+  FetchContent_Declare(
+    jsonprovider
+    URL "https://github.com/nlohmann/json/archive/${agent_json_lib_version_tag}.zip"
+  )
+  FetchContent_GetProperties(jsonprovider)
+  FetchContent_Populate(jsonprovider)
 
+  message(STATUS "  * Create targets for date library")
+  add_library(json INTERFACE)
+  target_include_directories(json INTERFACE "${jsonprovider_SOURCE_DIR}/single_include")
+endif()
