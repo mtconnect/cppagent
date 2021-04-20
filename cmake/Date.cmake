1463a780ead4631b8d87731eea3d6f84098cbb21
Will Sobel <will@wvsobel.llc>
2021-04-19 19:19:55 -0700

    First cut of vcpkg with pmm

--- cmake/Date.cmake
+++ cmake/Date.cmake
@@ -5,21 +5,3 @@
 # This automatically sets all required information such as include directories, definitions etc.   #
 ####################################################################################################
 
-set(agent_date_lib_version_tag "v2.4.1")
-
-if(NOT TARGET date)
-  message(STATUS "Make date library (${agent_date_lib_version_tag}) available.")
-  message(STATUS "  * Donwload date library")
-  include(FetchContent)
-  FetchContent_Declare(
-    dateprovider
-    URL "https://github.com/HowardHinnant/date/archive/${agent_date_lib_version_tag}.zip"
-  )
-  FetchContent_GetProperties(dateprovider)
-  FetchContent_Populate(dateprovider)
-  
-  add_library(date INTERFACE)
-  target_include_directories(date INTERFACE "${dateprovider_SOURCE_DIR}/include")
-#  set_target_properties(date PROPERTIES INTERFACE_SOURCES "${dateprovider_SOURCE_DIR}/src/tz.cpp" )
-#  set_target_properties(date PROPERTIES INTERFACE_COMPILE_DEFINITIONS "HAS_REMOTE_API=0")
-endif()
--- cmake/json.cmake
+++ cmake/json.cmake
@@ -5,20 +5,3 @@
 # This automatically sets all required information such as include directories, definitions etc.   #
 ####################################################################################################
 
-set(agent_json_lib_version_tag "v3.7.3")
-
-if(NOT TARGET json)
-  message(STATUS "Make JSON library (${agent_json_lib_version_tag}) available.")
-  message(STATUS "  * Download JSON library")
-  include(FetchContent)
-  FetchContent_Declare(
-    jsonprovider
-    URL "https://github.com/nlohmann/json/archive/${agent_json_lib_version_tag}.zip"
-  )
-  FetchContent_GetProperties(jsonprovider)
-  FetchContent_Populate(jsonprovider)
-
-  message(STATUS "  * Create targets for date library")
-  add_library(json INTERFACE)
-  target_include_directories(json INTERFACE "${jsonprovider_SOURCE_DIR}/single_include")
-endif()
