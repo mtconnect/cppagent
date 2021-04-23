4654228adb17e14406ed76633a912202e614a5b0
Will Sobel <will@wvsobel.llc>
2021-04-23 14:23:01 -0700

    Switched to conan. Need to test appveyor

--- cmake/Date.cmake
+++
@@ -1,7 +0,0 @@
-####################################################################################################
-#                 Providing date library - https://github.com/HowardHinnant/date                   #
-#                                                                                                  #
-# To use the date library simply link to it: target_link_libraries(<project> PRIVATE date).        #
-# This automatically sets all required information such as include directories, definitions etc.   #
-####################################################################################################
-
--- cmake/json.cmake
+++
@@ -1,7 +0,0 @@
-####################################################################################################
-#                    Providing JSON library - https://nlohmann.github.io/json                      #
-#                                                                                                  #
-# To use the JSON library simply link to it: target_link_libraries(<project> PRIVATE json).        #
-# This automatically sets all required information such as include directories, definitions etc.   #
-####################################################################################################
-
