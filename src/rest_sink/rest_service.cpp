f8809d71466286a47cb915d1ba63d73191a717f4
Will Sobel <will@wvsobel.llc>
2021-09-09 13:00:04 -0700

    Merged with master

--- src/rest_sink/rest_service.cpp
+++ src/rest_sink/rest_service.cpp
@@ -23,13 +23,15 @@
 namespace asio = boost::asio;
 using namespace std;
 
-namespace mtconnect {
+namespace mtconnect
+{
   using namespace configuration;
   using namespace observation;
   using namespace asset;
   using namespace device_model;
 
-  namespace rest_sink {
+  namespace rest_sink
+  {
     RestService::RestService(asio::io_context &context, SinkContractPtr &&contract,
                              const ConfigOptions &options)
       : Sink("RestService", move(contract)),
@@ -401,9 +403,10 @@ namespace mtconnect {
 
     struct AsyncSampleResponse
     {
-      AsyncSampleResponse(rest_sink::SessionPtr &session, boost::asio::io_context &context)
-        : m_session(session), m_observer(context), m_last(chrono::system_clock::now())
-      {}
+      AsyncSampleResponse(rest_sink::SessionPtr &session, boost::asio::io_context::strand &strand)
+        : m_session(session), m_observer(strand), m_last(chrono::system_clock::now())
+      {
+      }
 
       rest_sink::SessionPtr m_session;
       ofstream m_log;
@@ -439,7 +442,7 @@ namespace mtconnect {
         dev = checkDevice(printer, *device);
       }
 
-      auto asyncResponse = make_shared<AsyncSampleResponse>(session, m_context);
+      auto asyncResponse = make_shared<AsyncSampleResponse>(session, m_strand);
       asyncResponse->m_count = count;
       asyncResponse->m_printer = printer;
       asyncResponse->m_heartbeat = std::chrono::milliseconds(heartbeatIn);
@@ -466,9 +469,8 @@ namespace mtconnect {
       else
         asyncResponse->m_sequence = *from;
 
-      if (from >= m_circularBuffer.getSequence())
-        asyncResponse->m_endOfBuffer = true;
-
+      asyncResponse->m_endOfBuffer = from >= m_circularBuffer.getSequence();
+      
       asyncResponse->m_interval = chrono::milliseconds(interval);
       asyncResponse->m_logStreamData = m_logStreamData;
 
@@ -511,31 +513,17 @@ namespace mtconnect {
         LOG(warning) << ec.category().message(ec.value()) << ": " << ec.message();
         return;
       }
-
-      if (!asyncResponse->m_endOfBuffer)
-      {
-        // Wait to make sure the signal was actually signaled. We have observed that
-        // a signal can occur in rare conditions where there are multiple threads listening
-        // on separate condition variables and this pops out too soon. This will make sure
-        // observer was actually signaled and instead of throwing an error will wait again
-        // for the remaining hartbeat interval.
-        auto delta = chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now() -
-                                                                 asyncResponse->m_last);
-        if (delta < asyncResponse->m_interval)
-        {
-          asyncResponse->m_observer.wait(
-              asyncResponse->m_interval - delta,
-              asio::bind_executor(m_strand, boost::bind(&RestService::streamNextSampleChunk, this,
-                                                        asyncResponse, _1)));
-          return;
-        }
-      }
-
+      
       {
         std::lock_guard<CircularBuffer> lock(m_circularBuffer);
 
-        // See if the observer was signaled!
-        if (!asyncResponse->m_observer.wasSignaled())
+        if (!asyncResponse->m_endOfBuffer)
+        {
+          // Check if we are streaming chunks rapidly to catch up to the end of
+          // buffer. We will not delay between chunks in this case and write as
+          // rapidly as possible
+        }
+        else if (!asyncResponse->m_observer.wasSignaled())
         {
           // If nothing came out during the last wait, we may have still have advanced
           // the sequence number. We should reset the start to something closer to the
@@ -561,6 +549,7 @@ namespace mtconnect {
         uint64_t end(0ull);
         string content;
         asyncResponse->m_endOfBuffer = true;
+        
         // Check if we're falling too far behind. If we are, generate an
         // MTConnectError and return.
         if (asyncResponse->m_sequence < getFirstSequence())
@@ -587,6 +576,7 @@ namespace mtconnect {
           // begin filtering from where we left off.
           asyncResponse->m_sequence = end;
         }
+        
         if (m_logStreamData)
           asyncResponse->m_log << content << endl;
         asyncResponse->m_session->writeChunk(
@@ -600,7 +590,8 @@ namespace mtconnect {
     {
       AsyncCurrentResponse(rest_sink::SessionPtr session, asio::io_context &context)
         : m_session(session), m_timer(context)
-      {}
+      {
+      }
 
       rest_sink::SessionPtr m_session;
       chrono::milliseconds m_interval;
