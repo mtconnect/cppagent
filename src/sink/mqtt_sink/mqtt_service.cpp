38958c48443f77023ed5dcfe7c2b02c21ec936d0
rajani <rajani.atmuri@ge.com>
2022-04-15 10:01:16 -0400

    Revert "first try with MqttService_RegisterFactory"
    This reverts commit e8ccf77fbe49aeec7dd2c8dbab429a8eb07c2947.

--- src/sink/mqtt_sink/mqtt_service.cpp
+++ src/sink/mqtt_sink/mqtt_service.cpp
@@ -1,127 +1,111 @@
+////
+//// Copyright Copyright 2009-2021, AMT – The Association For Manufacturing Technology (“AMT”)
+//// All rights reserved.
+////
+////    Licensed under the Apache License, Version 2.0 (the "License");
+////    you may not use this file except in compliance with the License.
+////    You may obtain a copy of the License at
+////
+////       http://www.apache.org/licenses/LICENSE-2.0
+////
+////    Unless required by applicable law or agreed to in writing, software
+////    distributed under the License is distributed on an "AS IS" BASIS,
+////    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+////    See the License for the specific language governing permissions and
+////    limitations under the License.
+////
 //
-// Copyright Copyright 2009-2021, AMT – The Association For Manufacturing Technology (“AMT”)
-// All rights reserved.
+//#include "mqtt_service.hpp"
 //
-//    Licensed under the Apache License, Version 2.0 (the "License");
-//    you may not use this file except in compliance with the License.
-//    You may obtain a copy of the License at
+//#include "configuration/config_options.hpp"
 //
-//       http://www.apache.org/licenses/LICENSE-2.0
+//#include "entity/xml_parser.hpp"
 //
-//    Unless required by applicable law or agreed to in writing, software
-//    distributed under the License is distributed on an "AS IS" BASIS,
-//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//    See the License for the specific language governing permissions and
-//    limitations under the License.
+//#include "xml_printer.hpp"
 //
-
-#include "mqtt_service.hpp"
-
-#include "configuration/config_options.hpp"
-
-#include "entity/xml_parser.hpp"
-
-#include "xml_printer.hpp"
-
-namespace asio = boost::asio;
-using namespace std;
-namespace config = ::mtconnect::configuration;
-using ptree = boost::property_tree::ptree;
-
-namespace mtconnect 
-{
-  namespace sink 
-  {
-    namespace mqtt_sink 
-    {
-        //get obeservation in
-        //create a json printer
-        //call print 
-
-      MqttService::MqttService(boost::asio::io_context &context, sink::SinkContractPtr &&contract,
-                                 const ConfigOptions &options, const ptree &config)
-          : Sink("MqttService", move(contract))  , m_context(context), m_options(options)
-        {
-         
-        }
-
-      void MqttService::start() 
-      { 
-          
-      }
-
-      void MqttService::stop() 
-      {
-
-      }
-
-      uint64_t MqttService::publish(observation::ObservationPtr &observation) 
-      {
-          //get the data item from observation 
-          auto dataItem = observation->getDataItem();
-
-          //convert to json and send by mqtt
-
-          return 0; 
-      }
-
-      bool MqttService::publish(asset::AssetPtr asset) 
-      { 
-          return false; 
-      }
-
-      // Register the service with the sink factory
-      void MqttService::registerFactory(SinkFactory &factory)
-      {     
-        factory.registerFactory(
-            "MqttService",
-            [](const std::string &name, boost::asio::io_context &io, SinkContractPtr &&contract,
-               const ConfigOptions &options, const boost::property_tree::ptree &block) -> SinkPtr {
-              auto sink = std::make_shared<MqttService>(io, std::move(contract), options, block);
-              return sink;
-            });
-      }
-
-      void MqttService::printjSonEntity()
-      {
-        // create a json printer
-        //
-        // print json/xml particular entity
-      }
-
-        // Get the printer for a type
-      const std::string MqttService::acceptFormat(const std::string &accepts) const
-      {
-        std::stringstream list(accepts);
-        std::string accept;
-        while (std::getline(list, accept, ','))
-        {
-          for (const auto &p : m_sinkContract->getPrinters())
-          {
-            if (ends_with(accept, p.first))
-              return p.first;
-          }
-        }
-        return "xml";
-      }
-
-    const Printer *MqttService::printerForAccepts(const std::string &accepts) const
-      {
-        return m_sinkContract->getPrinter(acceptFormat(accepts));
-      }
-
-    string MqttService::printError(const Printer *printer, const string &errorCode,
-                                     const string &text) const
-      {
-        LOG(debug) << "Returning error " << errorCode << ": " << text;
-       /* if (printer)
-          return printer->printError(m_instanceId, m_circularBuffer.getBufferSize(),
-                                     m_circularBuffer.getSequence(), errorCode, text);
-        else*/
-
-          return errorCode + ": " + text;
-      }
-
+//namespace asio = boost::asio;
+//using namespace std;
+//namespace config = ::mtconnect::configuration;
+//using ptree = boost::property_tree::ptree;
+//
+//namespace mtconnect 
+//{
+//  namespace sink 
+//  {
+//    namespace mqtt_sink 
+//    {
+//        //get obeservation in
+//        //create a json printer
+//        //call print 
+//
+//      MqttService::MqttService(boost::asio::io_context &context, sink::SinkContractPtr &&contract,
+//                                 const ConfigOptions &options, const ptree &config)
+//          : Sink("MqttService", move(contract)), m_context(context), m_options(options)
+//      {
+//       
+//      }
+//
+//      void MqttService::start() 
+//      {
+//
+//      }
+//
+//      void MqttService::stop() 
+//      {
+//
+//      }
+//
+//      uint64_t MqttService::publish(observation::ObservationPtr &observation) 
+//      {
+//          //get the data item from observation 
+//          auto dataItem = observation->getDataItem();
+//
+//          //convert to json and send by mqtt
+//
+//          return 0; 
+//      }
+//
+//      bool MqttService::publish(asset::AssetPtr asset) 
+//      { 
+//          return false; 
+//      }
+//
+//      sink::SinkPtr MqttService::create(boost::asio::io_context &io,
+//                                        sink::SinkContractPtr &&contract,
+//                                        const ConfigOptions &options,
+//                                        const boost::property_tree::ptree &block)
+//      {
+//        return std::make_shared<MqttService>(io, std::move(contract), options, block);
+//      }
+//
+//       void MqttService::register_factory(const boost::property_tree::ptree &block,
+//                                         configuration::AgentConfiguration &config)
+//      {
+//        mtconnect::configuration::gAgentLogger = config.getLogger();
+//        PLUGIN_LOG(debug) << "Registering sink factory for MqttService";
+//        config.getSinkFactory().registerFactory("MqttService", &MqttService::create);
+//      }
+//
+//         // Register the service with the sink factory
+//      void MqttService::registerFactory(SinkFactory &factory)
+//      {     
+//
+//        factory.registerFactory(
+//            "MqttService",
+//            [](const std::string &name, boost::asio::io_context &io, SinkContractPtr &&contract,
+//               const ConfigOptions &options, const boost::property_tree::ptree &block) -> SinkPtr {
+//              auto sink = std::make_shared<MqttService>(io, std::move(contract), options, block);
+//              return sink;
+//            });
+//      }
+//
+//       void MqttService::printjSonEntity()
+//       {
+//           //create a json printer
+//           // 
+//           //print json particular entity
+//       }
+//
 //      /* std::shared_ptr<mtconnect::sink::mqtt_sink::MqttService> getMqttService()
 //       {
 //         using namespace mtconnect;
@@ -132,7 +116,7 @@ namespace mtconnect
 //         return mqttSink;
 //       }*/
 //
-    }  // namespace mqtt_sink
-  }    // namespace sink
-}  // namespace mtconnect
+//    }  // namespace mqtt_sink
+//  }    // namespace sink
+//}  // namespace mtconnect
 
--- src/sink/mqtt_sink/mqtt_service.hpp
+++ src/sink/mqtt_sink/mqtt_service.hpp
@@ -1,87 +1,84 @@
+////
+//// Copyright Copyright 2009-2021, AMT – The Association For Manufacturing Technology (“AMT”)
+//// All rights reserved.
+////
+////    Licensed under the Apache License, Version 2.0 (the "License");
+////    you may not use this file except in compliance with the License.
+////    You may obtain a copy of the License at
+////
+////       http://www.apache.org/licenses/LICENSE-2.0
+////
+////    Unless required by applicable law or agreed to in writing, software
+////    distributed under the License is distributed on an "AS IS" BASIS,
+////    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+////    See the License for the specific language governing permissions and
+////    limitations under the License.
+////
 //
-// Copyright Copyright 2009-2021, AMT – The Association For Manufacturing Technology (“AMT”)
-// All rights reserved.
+//#pragma once
 //
-//    Licensed under the Apache License, Version 2.0 (the "License");
-//    you may not use this file except in compliance with the License.
-//    You may obtain a copy of the License at
+//#include "boost/asio/io_context.hpp"
 //
-//       http://www.apache.org/licenses/LICENSE-2.0
+//#include <boost/dll/alias.hpp>
 //
-//    Unless required by applicable law or agreed to in writing, software
-//    distributed under the License is distributed on an "AS IS" BASIS,
-//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//    See the License for the specific language governing permissions and
-//    limitations under the License.
+//#include "configuration/agent_config.hpp"
 //
-
-#pragma once
-
-#include "boost/asio/io_context.hpp"
-
-#include <boost/dll/alias.hpp>
-
-#include "configuration/agent_config.hpp"
-
-#include "sink/sink.hpp"
-#include "utilities.hpp"
-
-using namespace std;
-
-namespace mtconnect 
-{
-  class XmlPrinter;
-
-  namespace sink 
-  {
-    namespace mqtt_sink 
-    {
-      class MqttService : public sink::Sink
-      {
-          //dynamic loading of sink
-
-      public:
-
-        MqttService(boost::asio::io_context &context, sink::SinkContractPtr &&contract,
-                    const ConfigOptions &options, const boost::property_tree::ptree &config);
-
-        ~MqttService() = default;
-
-        // Sink Methods
-        void start() override;
-
-        void stop() override;
-
-        uint64_t publish(observation::ObservationPtr &observation) override;
-
-        bool publish(asset::AssetPtr asset) override;
-
-        static void registerFactory(SinkFactory &factory);
-
-        void printjSonEntity();
-
-         // Get the printer for a type
-        const std::string acceptFormat(const std::string &accepts) const;
-
-        const Printer *printerForAccepts(const std::string &accepts) const;
-
-        // Output an XML Error
-        std::string printError(const Printer *printer, const std::string &errorCode,
-                               const std::string &text) const;
-
-       protected:
-
-        boost::asio::io_context &m_context;
-
-        ConfigOptions m_options;
-
-      };
+//#include "sink/sink.hpp"
+//
+//
+//using namespace std;
+//
+//namespace mtconnect 
+//{
+//  class XmlPrinter;
+//
+//  namespace sink 
+//  {
+//    namespace mqtt_sink 
+//    {
+//      class MqttService : public sink::Sink
+//      {
+//          //dynamic loading of sink
+//
+//      public:
+//
+//        MqttService(boost::asio::io_context &context, sink::SinkContractPtr &&contract,
+//                    const ConfigOptions &options, const boost::property_tree::ptree &config);
+//
+//        ~MqttService() = default;
+//
+//        // Sink Methods
+//        void start() override;
+//
+//        void stop() override;
+//
+//        uint64_t publish(observation::ObservationPtr &observation) override;
+//
+//        bool publish(asset::AssetPtr asset) override;
+//
+//        static sink::SinkPtr create(boost::asio::io_context &io,
+//                                    sink::SinkContractPtr &&contract, const ConfigOptions &options,
+//                                    const boost::property_tree::ptree &block);
+//
+//        static void register_factory(const boost::property_tree::ptree &block,
+//                                     configuration::AgentConfiguration &config);
+//
+//        static void registerFactory(SinkFactory &factory);
+//
+//        void printjSonEntity();
+//
+//         protected:
+//        // Loopback
+//        boost::asio::io_context &m_context;
+//
+//         ConfigOptions m_options;
+//
+//      };
 //
 //      BOOST_DLL_ALIAS(MqttService::register_factory, initialize_plugin)
 //
-    }  // namespace mqtt_sink
-  }    // namespace sink
-}  // namespace mtconnect
-
-
+//    }  // namespace mqtt_sink
+//  }    // namespace sink
+//}  // namespace mtconnect
+//
 
