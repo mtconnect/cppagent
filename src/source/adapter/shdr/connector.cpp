e19570a52b3b64f434dd5dcbe3d358f9cc96b3e4
Will Sobel <will@wvsobel.llc>
2022-08-24 13:17:11 -0700

    Removed unnecessary timer cancel

--- src/source/adapter/shdr/connector.cpp
+++ src/source/adapter/shdr/connector.cpp
@@ -240,7 +240,6 @@ namespace mtconnect::source::adapter::shdr {
   {
     NAMED_SCOPE("Connector::setReceiveTimeout");
 
-    m_receiveTimeout.cancel();
     m_receiveTimeout.expires_from_now(m_receiveTimeLimit);
     m_receiveTimeout.async_wait(asio::bind_executor(m_strand, [this](sys::error_code ec) {
       if (!ec)
