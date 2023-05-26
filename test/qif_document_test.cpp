ea0c08d4f93c5f1f195101122d85500d85302e40
Will Sobel <will@wvsobel.llc>
2023-05-26 08:03:06 -0700

    Changed the QIF representation as entities to use lists instead of properties for sub-elements.

--- test/qif_document_test.cpp
+++ test/qif_document_test.cpp
@@ -144,14 +144,20 @@ TEST_F(QIFDocumentTest, minimal_qif_definition)
 
   auto product = qif->get<EntityPtr>("Product");
   ASSERT_TRUE(product);
-  auto partSet = product->get<EntityPtr>("PartSet");
-  ASSERT_TRUE(partSet);
+  
+  auto partSetList = product->getListProperty();
+  ASSERT_EQ(2, partSetList.size());
+  auto partSet = *partSetList.begin();
   ASSERT_EQ("1"s, partSet->get<string>("N"));
-  auto part = partSet->get<EntityPtr>("Part");
+  
+  auto parts = partSet->getListProperty();
+  ASSERT_EQ(1, parts.size());
+  auto part = *parts.begin();
   ASSERT_EQ("1"s, part->get<string>("id"));
   ASSERT_EQ("Widget"s, part->get<string>("Name"));
   ASSERT_EQ("ed43400a-29bf-4ec6-b96c-e2f846eb6f00"s, part->get<string>("QPId"));
-  auto root = product->get<EntityPtr>("RootPart");
+  auto root = *(++partSetList.begin());
+  ASSERT_EQ("RootPart", root->getName());
   ASSERT_TRUE(root);
   ASSERT_EQ("1"s, root->get<string>("Id"));
 }
@@ -215,6 +221,10 @@ TEST_F(QIFDocumentTest, should_generate_json)
             <Name>Widget</Name>
             <QPId>ed43400a-29bf-4ec6-b96c-e2f846eb6f00</QPId>
           </Part>
+          <Part id="2">
+            <Name>Widget</Name>
+            <QPId>4cefd110-dd36-013b-f175-4e7f553bbb75</QPId>
+          </Part>
         </PartSet>
         <RootPart>
           <Id>1</Id>
@@ -239,19 +249,34 @@ TEST_F(QIFDocumentTest, should_generate_json)
   ASSERT_EQ(R"DOC({
   "QIFDocumentWrapper": {
     "QIFDocument": {
-      "Product": {
-        "PartSet": {
-          "N": "1",
-          "Part": {
-            "Name": "Widget",
-            "QPId": "ed43400a-29bf-4ec6-b96c-e2f846eb6f00",
-            "id": "1"
+      "Product": [
+        {
+          "PartSet": {
+            "list": [
+              {
+                "Part": {
+                  "Name": "Widget",
+                  "QPId": "ed43400a-29bf-4ec6-b96c-e2f846eb6f00",
+                  "id": "1"
+                }
+              },
+              {
+                "Part": {
+                  "Name": "Widget",
+                  "QPId": "4cefd110-dd36-013b-f175-4e7f553bbb75",
+                  "id": "2"
+                }
+              }
+            ],
+            "N": "1"
           }
         },
-        "RootPart": {
-          "Id": "1"
+        {
+          "RootPart": {
+            "Id": "1"
+          }
         }
-      },
+      ],
       "Version": {
         "ThisInstanceQPId": "fd43400a-29bf-4ec6-b96c-e2f846eb6ff6"
       },
@@ -266,4 +291,69 @@ TEST_F(QIFDocumentTest, should_generate_json)
   }
 })DOC"s,
             jdoc);
+}
+
+TEST_F(QIFDocumentTest, should_parse_document_with_multiple_same_named_elements)
+{
+  using namespace date;
+  const auto doc = R"DOC(
+<QIFDocumentWrapper assetId="30d278e0-c150-013a-c34d-4e7f553bbb76" qifDocumentType="PLAN">
+  <QIFDocument xmlns="http://qifstandards.org/xsd/qif2"
+     xmlns:q="http://qifstandards.org/xsd/qif2"
+     xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+     versionQIF="2.0.0"
+     xsi:schemaLocation="http://qifstandards.org/xsd/qif2 QIFApplications/QIFDocument.xsd">
+    <Version>
+      <ThisInstanceQPId>fd43400a-29bf-4ec6-b96c-e2f846eb6ff6</ThisInstanceQPId>
+    </Version>
+    <Product>
+      <PartSet N="2">
+        <Part id="1">
+          <Name>Widget</Name>
+          <QPId>ed43400a-29bf-4ec6-b96c-e2f846eb6f00</QPId>
+        </Part>
+        <Part id="2">
+          <Name>Widget</Name>
+          <QPId>4cefd110-dd36-013b-f175-4e7f553bbb75</QPId>
+        </Part>
+      </PartSet>
+      <RootPart>
+        <Id>1</Id>
+      </RootPart>
+    </Product>
+  </QIFDocument>
+</QIFDocumentWrapper>
+)DOC";
+
+  ErrorList errors;
+  entity::XmlParser parser;
+
+  auto entity = parser.parse(Asset::getRoot(), doc, errors);
+  ASSERT_EQ(0, errors.size());
+
+  auto asset = dynamic_cast<Asset *>(entity.get());
+  ASSERT_NE(nullptr, asset);
+
+  auto qif = asset->get<EntityPtr>("QIFDocument");
+  ASSERT_NE(nullptr, qif);
+
+  auto product = qif->get<EntityPtr>("Product");
+  ASSERT_TRUE(product);
+  auto &partSetList = product->getListProperty();
+  ASSERT_EQ(2, partSetList.size());
+  
+  auto &partSet = *partSetList.begin();
+  ASSERT_EQ("2"s, partSet->get<string>("N"));
+  auto parts = partSet->getListProperty();
+  ASSERT_EQ(2, parts.size());
+  
+  auto part = parts.begin();
+  ASSERT_EQ("1"s, (*part)->get<string>("id"));
+  ASSERT_EQ("Widget"s, (*part)->get<string>("Name"));
+  ASSERT_EQ("ed43400a-29bf-4ec6-b96c-e2f846eb6f00"s, (*part)->get<string>("QPId"));
+
+  part++;
+  ASSERT_EQ("2"s, (*part)->get<string>("id"));
+  ASSERT_EQ("Widget"s, (*part)->get<string>("Name"));
+  ASSERT_EQ("4cefd110-dd36-013b-f175-4e7f553bbb75"s, (*part)->get<string>("QPId"));
 }
