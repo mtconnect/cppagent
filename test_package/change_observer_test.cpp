5fc564d398d0ea91ca732ee45a7441a12e98daef
rajwork9 <101668987+rajwork9@users.noreply.github.com>
2023-09-25 11:59:03 -0500

    build fix for windows

--- test_package/change_observer_test.cpp
+++ test_package/change_observer_test.cpp
@@ -304,9 +304,7 @@ namespace mtconnect {
     ASSERT_FALSE(called);
 
     expected = addObservations(1);
-    ASSERT_EQ(4ull, expected);
-
-    m_context.run_for(200ms);
+    ASSERT_EQ(4ull, expected);       
     ASSERT_FALSE(called);
 
     m_context.run_for(200ms);
@@ -347,10 +345,9 @@ namespace mtconnect {
 
     expected = addObservations(1);
     ASSERT_EQ(4ull, expected);
-
-    m_context.run_for(100ms);
     ASSERT_FALSE(called);
 
+    m_context.run_for(100ms);
     waitFor([&called]() { return called; });
     ASSERT_TRUE(called);
   }
