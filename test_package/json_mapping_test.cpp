c5b7d3012adb7266f91a5cbb0c8d91aed1e4829a
rajwork9 <101668987+rajwork9@users.noreply.github.com>
2024-10-03 00:10:25 -0500

    Update json_mapping_test.cpp
    unit test case - ignore_Invalid_make_DataItem, ignore_Invalid_Tag_DataItem

--- test_package/json_mapping_test.cpp
+++ test_package/json_mapping_test.cpp
@@ -1138,5 +1138,86 @@ TEST_F(JsonMappingTest, should_skip_erroneous_table)
   ASSERT_EQ("MANUAL", obs->getValue<string>());
 }
 
+// if the tag "blah" is not found in the device file, none of the other tags get processed
+TEST_F(JsonMappingTest, should_ignore_Invalid_Tag_DataItem)
+{
+  auto dev = makeDevice("Device", {{"id", "device"s}, {"name", "device"s}, {"uuid", "device"s}});
+  makeDataItem("device", {{"id", "a"s}, {"type", "EXECUTION"s}, {"category", "EVENT"s}});
+  makeDataItem("device", {{"id", "c"s}, {"type", "CONTROLLER_MODE"s}, {"category", "EVENT"s}});
+
+  Properties props {{"VALUE", R"(
+{
+  "timestamp": "2023-11-09T11:20:00Z",
+"c": "BLAH",
+  "a": {
+      "r1": {
+        "k1": 123.45
+      },
+      "r2": {
+        "k2": "ABCDEF",
+        "k3": 6789
+      }
+    },
+   "b": "MANUAL"
+})"s}};
+
+  auto jmsg = std::make_shared<JsonMessage>("JsonMessage", props);
+  jmsg->m_device = dev;
+
+  auto res = (*m_mapper)(std::move(jmsg));
+  ASSERT_TRUE(res);
+
+  auto value = res->getValue();
+  ASSERT_TRUE(std::holds_alternative<EntityList>(value));
+  auto list = get<EntityList>(value);
+  ASSERT_EQ(1, list.size());
+
+  auto obs = dynamic_pointer_cast<Observation>(list.front());
+  ASSERT_TRUE(obs);
+  ASSERT_EQ("ControllerMode", obs->getName());
+  ASSERT_EQ("c", obs->getDataItem()->getId());
+  ASSERT_EQ("BLAH", obs->getValue<string>());
+}
+
+TEST_F(JsonMappingTest, should_ignore_Invalid_make_DataItem)
+{
+  auto dev = makeDevice("Device", {{"id", "device"s}, {"name", "device"s}, {"uuid", "device"s}});
+  makeDataItem("device", {{"id", "a"s}, {"type", "EXECUTION"s}, {"category", "EVENT"s}});
+  makeDataItem("device", {{"id", "b"s}, {"blah", "BLAH"s}, {"category", "EVENT"s}});
+
+  Properties props {{"VALUE", R"(
+{
+  "timestamp": "2023-11-09T11:20:00Z",
+"c": "Blah",
+  "a": {
+      "r1": {
+        "k1": 123.45
+      },
+      "r2": {
+        "k2": "ABCDEF",
+        "k3": 6789
+      }
+    },
+   "b": "MANUAL"
+})"s}};
+
+  auto jmsg = std::make_shared<JsonMessage>("JsonMessage", props);
+  jmsg->m_device = dev;
+
+  auto res = (*m_mapper)(std::move(jmsg));
+  ASSERT_TRUE(res);
+
+  auto value = res->getValue();
+  ASSERT_TRUE(std::holds_alternative<EntityList>(value));
+  auto list = get<EntityList>(value);
+  ASSERT_EQ(1, list.size());
+
+  auto obs = dynamic_pointer_cast<Observation>(list.front());
+  ASSERT_TRUE(obs);
+  ASSERT_EQ("ControllerMode", obs->getName());
+  ASSERT_EQ("b", obs->getDataItem()->getId());
+  ASSERT_EQ("MANUAL", obs->getValue<string>());
+}
+
 /// @test verify the json mapper can an asset in json
 TEST_F(JsonMappingTest, should_parse_json_asset) { GTEST_SKIP(); }
