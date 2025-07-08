f722ee922a124781fb2ef0cbca04606d4c960ff5
Will Sobel <will@wvsobel.llc>
2025-07-08 11:47:19 +0200

    Fixed conanfile for mruby due to changes in conan version 2.18

--- conan/mruby/conanfile.py
+++ conan/mruby/conanfile.py
@@ -195,9 +195,10 @@ end
 
         buf = io.StringIO()
         self.run("{} --cflags".format(ruby), stdout=buf, shell=True)
-        self.cpp_info.defines = [d[2:] for d in buf.getvalue().split(' ') if d.startswith('/D') or d.startswith('-D')]
 
-        self.conf_info.define('mruby', 'ON')
+        defines = [d[2:] for d in buf.getvalue().split(' ') if d.startswith('/D') or d.startswith('-D')]
+        defines.append('mruby=ON')
+        self.cpp_info.defines = defines
 
         self.cpp_info.bindirs = ["bin"]
         if self.settings.os == 'Windows':
